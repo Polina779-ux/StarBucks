d6443c5922cb3adaf905f39f0f0aab58b208be41
Polina <pparanich@ukr.net>
2025-04-09 21:33:04 +0300

    feat: function forEach

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,33 @@
 const burger = document.querySelector('.btn-burger');
-const mobileContainer = document.querySelector('.mobile-container')
+const mobileContainer = document.querySelector('.mobile-container');
 
-burger.addEventListener('click', ()=> {
-    burger.classList.toggle('active')
-    mobileContainer.classList.toggle('show')
+function showMobileContainer() {
+    mobileContainer.classList.toggle('d-block'); 
+    burger.classList.toggle('active');
+}
+
+burger.addEventListener('click', showMobileContainer);
+
+const label = document.querySelectorAll ('.label')
+
+label.forEach(item => {
+       item.addEventListener('mousedown', function() {
+            item.classList.add('hovered')
+         })
+    
+        item.addEventListener('mouseup', function() {
+             item.classList.remove('hovered')
+         })
+     })
+
+const btn = document.querySelectorAll('.btn')
+
+btn.forEach(item => {
+    item.addEventListener('mouseover', function() {
+        item.classList.add('hovered')
+    })
+
+    item.addEventListener('mouseout', function() {
+        item.classList.remove('hovered')
+    })
 })
