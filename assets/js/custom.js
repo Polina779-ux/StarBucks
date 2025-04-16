f71ef6d6c6344c3a0b13953787cccbba7a09d4a6
Polina <pparanich@ukr.net>
2025-04-17 01:38:39 +0300

    feat: videoplayer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,25 +8,3 @@ function showMobileContainer() {
 
 burger.addEventListener('click', showMobileContainer);
 
-const label = document.querySelectorAll ('.label')
-label.forEach(item => {
-       item.addEventListener('mousedown', function() {
-            item.classList.add('hovered')
-         })
-    
-        item.addEventListener('mouseup', function() {
-             item.classList.remove('hovered')
-         })
-     })
-
-const btn = document.querySelectorAll('.btn')
-
-btn.forEach(item => {
-    item.addEventListener('mouseover', function() {
-        item.classList.add('hovered')
-    })
-
-    item.addEventListener('mouseout', function() {
-        item.classList.remove('hovered')
-    })
-})
